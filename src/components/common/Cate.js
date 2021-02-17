2a2a010428d62d2f2891f68710d23be13e6747f2
eomji <anipap2@naver.com>
2021-02-17 14:39:58 +0900

    Add responsible-mini-category

--- src/components/common/Cate.js
+++ src/components/common/Cate.js
@@ -2,6 +2,25 @@ import React from 'react';
 import styled from 'styled-components';
 import { MdKeyboardArrowRight } from 'react-icons/md';
 
+const PostCate = styled.ul`
+  display: flex;
+  gap: 6px;
+  span + span {
+    margin-left: 4px;
+  }
+  @media ${({ theme }) => theme.mobile} {
+    span + span {
+      display: none;
+    }
+  }
+`;
+const PreCate = styled.ul`
+  display: flex;
+  gap: 6px;
+  @media ${({ theme }) => theme.mobile} {
+    display: none;
+  }
+`;
 const CateBox = styled.ul`
   display: flex;
   gap: 6px;
@@ -10,31 +29,34 @@ const CateBox = styled.ul`
   border-bottom: 1px solid #dfdfdf;
   font-size: 12px;
   color: #484848;
-  li {
-    display: flex;
-    gap: 4px;
-  }
-  @media ${({ theme }) => theme.mobile} {
-  }
 `;
 
 export default function Cate({ bCate, sCate, title, summary }) {
   return (
     <CateBox>
-      <li>제품</li>
       <li>
-        <MdKeyboardArrowRight />
+        <PreCate>
+          <li>제품</li>
+          <li>
+            <MdKeyboardArrowRight />
+          </li>
+          <li>{bCate.name}</li>
+          <li>
+            <MdKeyboardArrowRight />
+          </li>
+        </PreCate>
       </li>
-      <li>{bCate.name}</li>
       <li>
-        <MdKeyboardArrowRight />
-      </li>
-      <li>{sCate.name}</li>
-      <li>
-        <MdKeyboardArrowRight />
-      </li>
-      <li>
-        <span>{`${title} ${summary}`}</span>
+        <PostCate>
+          <li>{sCate.name}</li>
+          <li>
+            <MdKeyboardArrowRight />
+          </li>
+          <li>
+            <span>{title}</span>
+            <span>{summary}</span>
+          </li>
+        </PostCate>
       </li>
     </CateBox>
   );
